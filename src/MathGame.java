bf39e65374454150412269a90c9fec9db66b4fa5
Tom Campbell <65591392+tomcampbell23@users.noreply.github.com>
2024-03-08 13:33:02 +1000

    Update

--- src/MathGame.java
+++ src/MathGame.java
@@ -8,24 +8,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MathGame {
-    public static void main(String[] args) throws Exception {
+    static int askQuestion() {
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
+        int x = rand.nextInt(1,13);
+        int y = rand.nextInt(1,13);
+        int correct = 0;
+        if (x > y) {
+            System.out.printf("What is %2d - %2d? %n", x, y);
+            correct = x - y;
+        } else {
+            System.out.printf("What is %2d + %2d? %n", x, y);
+            correct = x + y;
+        }
+        int response = input.nextInt();
+        //input.close();
+        if (response == correct) {
+            System.out.printf("That's correct! %n");
+            return 1;
+        } else {
+            System.out.printf("That's not quite right. %n");
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i < 10; i++) {
-            int x = rand.nextInt(1,13);
-            int y = rand.nextInt(1,13);
-            System.out.printf("What is %2d + %2d? %n", x, y);
-            int response = input.nextInt();
-            int correct = x + y;
-            if (response == correct) {
-                System.out.printf("That's correct! %n");
-                count += 1;
-            } else {
-                System.out.printf("That's not quite right. %n");
-            }
+            int result = askQuestion();
+            count += result;
         }
-        input.close();
         if (count >= 8) {
             System.out.printf("You got %2d out of 9. Congratulations! %n", count);
         } else {
